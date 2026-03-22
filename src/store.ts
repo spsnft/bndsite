ec0728181a5c34ace58b83d15d0465886aefe020
spsnft <87615686+spsnft@users.noreply.github.com>
2026-03-22 23:38:50 +0700

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,6 @@ export const useCart = create<any>()(
     (set, get) => ({
       items: [],
       addItem: (newItem: any) => set((state: any) => {
-        // Учитываем ID + ВЕС, чтобы разные граммовки одного сорта были разными позициями
         const ex = state.items.findIndex((i: any) => i.id === newItem.id && i.weight === newItem.weight);
         if (ex > -1) {
           const newItems = [...state.items];
@@ -17,7 +16,7 @@ export const useCart = create<any>()(
       }),
       removeItem: (id: string, weight: string) => set((state: any) => ({
         items: state.items.filter((i: any) => !(i.id === id && i.weight === weight))
-      }),),
+      })),
       updateQuantity: (id: string, weight: string, delta: number) => set((state: any) => {
         const newItems = state.items.map((i: any) => {
           if (i.id === id && i.weight === weight) {
@@ -31,6 +30,6 @@ export const useCart = create<any>()(
       clearCart: () => set({ items: [] }),
       getTotal: () => get().items.reduce((acc: number, item: any) => acc + (item.price * item.quantity), 0)
     }),
-    { name: "bnd-cart-v12" } // Ключ один на весь домен
+    { name: "bnd-cart-v12" }
   )
 )
