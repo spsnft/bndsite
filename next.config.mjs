7ed69b1b36e7de0f2c6a5c4bd81cd43b8985cb9b
spsnft <87615686+spsnft@users.noreply.github.com>
2026-03-28 21:59:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,19 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  // Оптимизация CSS (убирает блокирующие ресурсы)
+  experimental: {
+    optimizeCss: true, 
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // Добавь это, раз мы используем Cloudinary для лого
       },
     ],
   },
